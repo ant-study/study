739463f608a63445b8bdf67bfa1d7a92adfaab88
hankk <hankk20@gmail.com>
2020-09-03 17:41:31 +0900

    reflection sample 추가

---
+++ src/main/java/kr/co/ant/study/hankwangsu/reflection/MemberController.java
@@ -0,0 +1,43 @@
+package kr.co.ant.study.hankwangsu.reflection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemberController {
+	
+	private static Map<String, String> testData;
+	private static Map<String, String> testData2;
+	static {
+		testData = new HashMap<String, String>();
+		testData.put("A001", "한광수");
+		testData.put("A002", "송정우");
+		
+		testData2 = new HashMap<String, String>();
+		testData2.put("A001", "서울시");
+		testData2.put("A002", "파주시");
+	}
+	
+	
+	public String getMemberName(SearchVO vo) {
+		
+		String searchId = vo.getMemberId();
+		
+		System.out.println("Search MemberName Input MemberID ::: "+vo.getMemberId());
+		
+		return testData.get(vo.getMemberId());
+	}
+	
+	public String getMemberAddress(SearchVO vo) {
+		
+		String searchId = vo.getMemberId();
+		
+		System.out.println("Search Member Address Input MemberID ::: "+vo.getMemberId());
+		
+		return testData2.get(vo.getMemberId());
+	}
+	
+	public String getMemberPhone(SearchVO vo) {
+		return "phone";
+	}
+
+}
---
+++ src/main/java/kr/co/ant/study/hankwangsu/reflection/SearchVO.java
@@ -0,0 +1,15 @@
+package kr.co.ant.study.hankwangsu.reflection;
+
+public class SearchVO {
+
+	private String memberId;
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	
+}
