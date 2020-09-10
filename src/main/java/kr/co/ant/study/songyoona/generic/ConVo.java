5d3c3abf02ca759064e843faf00e7a4062f8a8d8
Song_YooNa <yoos417@ant-soft.co.kr>
2020-09-10 18:27:40 +0900

    제네릭 숙제

---
+++ src/main/java/kr/co/ant/study/songyoona/generic/ConVo.java
@@ -0,0 +1,14 @@
+/**
+ * Author : yooS
+ * Create Date : 2020. 9. 10.
+ */
+package kr.co.ant.study.songyoona.generic;
+
+/**
+ * @description : 
+ * @author : yooS
+ * @createDate : 2020. 9. 10.
+ */
+public interface ConVo {
+
+}
