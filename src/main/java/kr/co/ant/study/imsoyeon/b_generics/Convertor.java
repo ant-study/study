4089bb73779367a9c162a4ca8e2321620283d913
Im_SoYeon <syrong@ant-soft.co.kr>
2020-09-11 17:34:11 +0900

    정리

--- src/main/java/kr/co/ant/study/imsoyeon/b_generics/Convertor.java
+++ src/main/java/kr/co/ant/study/imsoyeon/b_generics/Convertor.java
@@ -27,7 +27,13 @@ public class Convertor {
 	 * @return
 	 * @throws Exception
 	 */
-//	Q1. 왜 static이 꼭 붙어여할까? 이 클래스에서만 재사용할거니까?
+//	Q1. 왜 static이 꼭 붙어여할까?
+//			→ main 메소드에서 new Convertor() 안하려고
+//			→ 객체생성을 하지 않고도 메소드에 접근해서 호출할 수 있다
+//			→ 컴파일시 static은 먼저 메모리에 생성됨 	(	정적 메모리에 올라간다는데 그게 어디? Heap? 생명주기 : 프로그램 시작&끝)
+//			→ non-static(인스턴스 변수)는 소속 클래스의 객체가 생성될 때, 생성됨 	(	Stack에 생성 생명주기 : 객체랑 같이	)  
+//			→ static 메소드 안에서는 non-static 메소드는 객체 생성없이 사용할 수 없음. 없으니까?
+	
 //	Class 2개를 제네릭 인자로 넣을 수 없음. Class는 1개, Interface는 n개 설정 가능
 	public static <T extends Object>T toVO(Map map, Class<T> clazz) throws Exception{
 		T obj = clazz.newInstance();
