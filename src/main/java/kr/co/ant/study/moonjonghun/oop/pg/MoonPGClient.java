5b8b59eb65a9ee827a0f2dadbfd67abb06894763
moon <moon@192.168.123.106>
2020-09-14 00:14:30 +0900

    pg가 어디갔을까요...

---
+++ src/main/java/kr/co/ant/study/moonjonghun/oop/pg/MoonPGClient.java
@@ -0,0 +1,17 @@
+package kr.co.ant.study.moonjonghun.oop.pg;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+public class MoonPGClient {
+	
+	@Autowired
+	ObjectMapper mapper;
+
+	public void doPayment(Object obj) {
+		
+	}
+}
