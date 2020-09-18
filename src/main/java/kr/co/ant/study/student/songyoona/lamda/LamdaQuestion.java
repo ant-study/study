c46912c2c1bb2637bb022e5f4e86f7431cd367bc
Song_YooNa <yoos417@ant-soft.co.kr>
2020-09-18 17:34:32 +0900

    람다 예제

---
+++ src/main/java/kr/co/ant/study/student/songyoona/lamda/LamdaQuestion.java
@@ -0,0 +1,29 @@
+package kr.co.ant.study.student.songyoona.lamda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class LamdaQuestion {
+	private static final String PREFIX = "my";
+
+	public static void exe(Function<String, String> f) {
+		List<String> list = Arrays.asList("name", "job", "address");
+		for(String s : list) {
+			System.out.println(f.apply(s));
+		}
+	}
+
+
+
+	public static void main(String[] args) {
+		//exe를 호출 해서 my_name, my_job, my_address 라고 출력되게 구현 해보세요
+
+	    // f = PREFIX + "_" 가 합쳐진 함수
+	    Function<String,String> words = (list)-> PREFIX + "_" + list;
+	    exe(words);
+	}
+
+
+}
