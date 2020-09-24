49d86aa9f689428cf912e45054854f3bd2bc0a8c
mjSeo <juuxmee@gmail.com>
2020-09-24 09:52:01 +0900

    member명 변경

--- src/main/java/kr/co/ant/study/student/seomyeongjoo/jpa/repository/MemberMRepository.java
+++ src/main/java/kr/co/ant/study/student/seomyeongjoo/jpa/repository/MemberMRepository.java
@@ -1,17 +1,18 @@
 package kr.co.ant.study.student.seomyeongjoo.jpa.repository;
 
-import kr.co.ant.study.student.seomyeongjoo.jpa.domain.Member;
+
+import kr.co.ant.study.student.seomyeongjoo.jpa.domain.Member_m;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 
 @Repository
-public class MemberRepository {
+public class MemberMRepository {
     @Autowired
     private EntityManager em;
 
-    public void save(Member member){
+    public void save(Member_m member){
         em.persist(member);
     }
 }
