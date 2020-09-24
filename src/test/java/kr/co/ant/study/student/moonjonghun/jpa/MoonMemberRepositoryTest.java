86410d32956b5d3cac5834d56ef70eb6e0e3af31
문종훈 <lune6669@ant-soft.co.kr>
2020-09-24 15:09:08 +0900

    hst Entity 추가 , Entity간 column join

--- src/test/java/kr/co/ant/study/student/moonjonghun/jpa/MoonMemberRepositoryTest.java
+++ src/test/java/kr/co/ant/study/student/moonjonghun/jpa/MoonMemberRepositoryTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 
 import kr.co.ant.study.student.moonjonghun.jpa.DAO.MoonInit;
+import kr.co.ant.study.student.moonjonghun.jpa.DAO.MoonInitHst;
 
 @EntityScan(basePackages = "kr.co.ant.study.student.moonjonghun.jpa")
 @DataJpaTest
@@ -46,6 +47,22 @@ class MoonMemberRepositoryTest {
 		init.setStockQty(1.00001);
 		
 		repository.save(init);
+		
+//		MoonInitHst hst = new MoonInitHst();
+		
+//		if(init.getInitId() <= 0 ) {
+		//init_id를 가져와서 hst에 조회를 한다음 값이 없으면 setSeq(1)
+		//조회는 어떻게 하지... 엔티티매니저님 도와주세요...
+//		if(true) {
+//			hst.setSeq(1);
+//		}else{
+//			//init
+//		};
+		
+		
+		
 	}
+	
+	
 
 }
