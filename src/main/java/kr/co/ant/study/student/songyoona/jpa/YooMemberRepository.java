9adb05a11e8bab13c9cd951e8d5f5f06f41485dd
Song_YooNa <yoos417@ant-soft.co.kr>
2020-09-28 14:46:58 +0900

    trigger역할 적용:insert만 됌..

--- src/main/java/kr/co/ant/study/student/songyoona/jpa/YooMemberRepository.java
+++ src/main/java/kr/co/ant/study/student/songyoona/jpa/YooMemberRepository.java
@@ -4,15 +4,16 @@
  */
 package kr.co.ant.study.student.songyoona.jpa;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import javax.persistence.EntityManager;
-import javax.persistence.PrePersist;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import kr.co.ant.study.student.songyoona.jpa.domain.YooMember;
 import kr.co.ant.study.student.songyoona.jpa.domain.YooSaltbInit01;
-import kr.co.ant.study.student.songyoona.jpa.domain.YooSaltbInit01Hst;
 
 /**
  * @description :
@@ -25,10 +26,31 @@ public class YooMemberRepository {
     @Autowired
     private EntityManager em;
 
+    /* 자동생성된 해당 키값 바로 get */
+    public long getGeneratedKey(final Statement statement) throws SQLException {
+        ResultSet resultSet = statement.getGeneratedKeys();
+        return resultSet.next() ? resultSet.getLong(1) : 0L;
+      }
+
     public <T> void save(T t) {
         em.persist(t);
     }
 
+    public <T> void merge(T entity) {
+        em.merge(entity);
+    }
+
+    public <T> void update(T t) {
+        em.merge(t);
+    }
+
+    public <T> void remove(T t) {
+        //em.delete(t);
+    }
+
+    public <T> void select(T t) {
+        em.find(YooSaltbInit01.class, t);
+    }
 //    public void save(YooMember member) {
 //        em.persist(member);
 //    }
--- src/main/java/kr/co/ant/study/student/songyoona/jpa/domain/YooSaltbInit01Hst.java
+++ src/main/java/kr/co/ant/study/student/songyoona/jpa/domain/YooSaltbInit01Hst.java
@@ -15,6 +15,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -24,6 +25,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @description :
@@ -31,24 +33,27 @@ import lombok.Setter;
  * @createDate : 2020. 9. 23.
  */
 @Entity
-@Table(name="yoo_saltb_init01_hst", uniqueConstraints={@UniqueConstraint(columnNames={"INIT_ID","seq","EVENT_DSCD"})})//
+@Table(name="yoo_saltb_init01_hst", uniqueConstraints={@UniqueConstraint(name="init_id", columnNames={"INIT_ID","seq","EVENT_DSCD"})})//
 @DynamicUpdate  //  value가 변경된 컬럼만 update쳐라
 @DiscriminatorValue("hst")
-@Getter @Setter
+@Getter @Setter @Slf4j
 public class YooSaltbInit01Hst{
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(nullable = false, length = 25, columnDefinition="BIGINT unsigned")
     private Long initHstId;
 
-    //@OneToMany @JoinColumn(name = "init_id")
-    //@ManyToMany(mappedBy = "initId")
-    //@Column(name = "init_id", nullable = false, length = 19, columnDefinition="BIGINT unsigned")
-
-    // 자식이니까 many이고 부모는 one
-    @ManyToOne @JoinColumn(name = "init_id", nullable=false)
-    private YooSaltbInit01 yooSaltbInit01;
 //    @Column(name = "INIT_ID", nullable = false, length = 19, columnDefinition="BIGINT unsigned")
 //    private Long initId;
+    // 자식이니까 many이고 부모는 one
+    @ManyToOne @JoinColumn(name = "init_id", nullable=false)
+    private YooSaltbInit01 initId;
+    public void setYooSaltbInit01(YooSaltbInit01 initId) {
+        this.initId = initId;
+        // 무한루프에 빠지지 않도록 체크.
+        if(!initId.getYooInitHistories().contains(this)) {
+            initId.getYooInitHistories().add(this);
+        }
+    }
 
     @Column(nullable = false, length = 19, columnDefinition="int unsigned")
     private int seq;
@@ -87,5 +92,12 @@ public class YooSaltbInit01Hst{
 
     @Column(name = "sys_upd_date")
     private LocalDateTime sysUpdDate;
+
+    @PreUpdate
+    public void preUpdate() {
+        log.info("Init01Hst PreUpdate 한다!!!");
+        this.EventDscd = "D";
+        this.sysUpdId = "UpYoo";
+    }
 
 }
